712569ba3478b8cb4a35d7151d95345406c84181
Melanie <mel.tschiersch@gmail.com>
2020-01-21 13:52:54 +0100

    Adaptation code

--- Schreibtisch/MSNE/NISE/scripts/eval_Adaptation.py
+++ Schreibtisch/MSNE/NISE/scripts/eval_Adaptation.py
@@ -31,20 +31,33 @@ def get_results(files):
     results=[]
     for i,s1 in enumerate(s):
         try:
-            results.append([float(s1.split(" ")[0]),float(s1.split(" ")[1]),float(s1.split(" ")[2])])
+            results.append([float(s1.split(" ")[0]),float(s1.split(" ")[1]),float(s1.split(" ")[2]),float(s1.split(" ")[3]),float(s1.split(" ")[4]),float(s1.split(" ")[5]),float(s1.split(" ")[6])])
         except:
             continue
         
     results=np.array(results)
     return results
 
-filename_reg = '/home/melanie/Schreibtisch/MSNE/NISE/results/SeveralBumps.txt'
+filename_reg = '/home/melanie/Schreibtisch/MSNE/NISE/results/SeveralBumps2.txt'
 
 results = get_results(filename_reg)
+print(results)
+setmax = results[0,5]
+trialmax = results[0,6]
+
+ordered_res = np.zeros((int(setmax), int(trialmax), 7))
+for sets in range(0,int(setmax)):
+    ordered_res[sets, :, :] = results[int(sets)*int(trialmax):int(sets*trialmax+trialmax),:]
+
+mean_trial = np.zeros((int(trialmax),7))
+std_trial = np.zeros((int(trialmax),7))
+for trial in range(0, int(trialmax)):
+    mean_trial[trial,:] = np.mean(ordered_res[:,trial,:],axis=0)
+    std_trial[trial,:] = np.std(ordered_res[:,trial,:],axis=0)
 
 plt.rcParams.update({'font.size': 14})
-plt.plot(np.linspace(1, len(results), len(results)), results[:,2], linestyle='none', color='darkblue', linewidth=4, Marker='o', MarkerSize='10')
+plt.errorbar(np.linspace(1, int(trialmax), int(trialmax)),mean_trial[:,2], std_trial[:,2], linestyle='none', color='darkblue', linewidth=4, Marker='o', MarkerSize='10')
 plt.axhline(y=0, linestyle='--', color='k')
 plt.ylabel('Error in endposition °', FontSize= 16)
 plt.xlabel('# of trial', FontSize= 16)
-#plt.savefig('/home/melanie/Schreibtisch/MSNE/NISE/results/Adaptation_Force2Learning1.png', dpi=300)
+#plt.savefig('/home/melanie/Schreibtisch/MSNE/NISE/results/Adaptation_Average5Sets.png', dpi=300)
